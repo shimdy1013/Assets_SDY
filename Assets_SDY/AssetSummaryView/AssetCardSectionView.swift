81aec930110d4c1d3165444a395f6b8692c83592
shimdy1013 <shimdy1013@naver.com>
2023-04-28 23:34:15 +0900

    AssetView 완성

---
+++ Assets_SDY/AssetSummaryView/AssetCardSectionView.swift
@@ -0,0 +1,20 @@
+//
+//  AssetCardSectionView.swift
+//  Assets_SDY
+//
+//  Created by 심두용 on 2023/04/28.
+//
+
+import SwiftUI
+
+struct AssetCardSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AssetCardSectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetCardSectionView()
+    }
+}
---
+++ Assets_SDY/AssetSummaryView/TabMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  TabMenuView.swift
+//  Assets_SDY
+//
+//  Created by 심두용 on 2023/04/28.
+//
+
+import SwiftUI
+
+struct TabMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabMenuView()
+    }
+}
